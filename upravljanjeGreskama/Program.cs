f08a6608571f7864503e6319da6a58b981c9e019
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-10-18 08:25:59 +0200

    Add files via upload

---
+++ upravljanjeGreskama/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upravljanjeGreskama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godRod;
+            bool kontrola = false;
+
+            Console.Write("Upiši svoju godinu rođenja: ");
+            do{
+
+                try
+                {
+                    godRod = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Rođen si " + godRod + " godine");
+                    kontrola = true;
+                }
+
+                catch (Exception greska)
+                {
+                    Console.WriteLine(greska.Message);
+                    kontrola = false;
+                }
+
+            } while (kontrola == false);
+
+            Console.ReadKey();
+            }
+    }
+}
